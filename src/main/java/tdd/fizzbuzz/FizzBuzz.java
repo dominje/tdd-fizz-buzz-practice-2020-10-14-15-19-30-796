c9324abebe9887686589f57b78225b67fafe69ad
dominje <jericah.domingo@oocl.com>
2020-10-15 14:02:05 +0800

    [fizzbuzz][jericah]refactor: replaced variables to constants

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -2,9 +2,12 @@ package tdd.fizzbuzz;
 
 public class FizzBuzz {
 
+    public static final String FIZZ = "Fizz";
+    public static final int MODULO_3 = 3;
+
     public String countOff(int inputNumber) {
-        if(inputNumber % 3 ==0){
-            return "Fizz";
+        if(inputNumber % MODULO_3 ==0){
+            return FIZZ;
         }
         return "";
     }
