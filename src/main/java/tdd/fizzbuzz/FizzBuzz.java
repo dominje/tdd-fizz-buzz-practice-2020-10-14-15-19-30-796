300afebaa43e34ea126b026bc7542e776fa882ae
dominje <jericah.domingo@oocl.com>
2020-10-15 14:38:48 +0800

    [fizzbuzz][jericah]refactor: removed redundant constants

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -2,22 +2,20 @@ package tdd.fizzbuzz;
 
 public class FizzBuzz {
 
-    private static final String FIZZ = "Fizz";
     private static final int MODULO_3 = 3;
-    private static final String BUZZ = "Buzz";
     private static final int MODULO_5 = 5;
     private static final int MODULO_7 = 7;
+    private static final String FIZZ = "Fizz";
+    private static final String BUZZ = "Buzz";
     private static final String WHIZZ = "Whizz";
-    private static final String FIZZ_BUZZ = "FizzBuzz";
-    public static final String FIZZ_WHIZZ = "FizzWhizz";
 
     public String countOff(int inputNumber) {
         if(inputNumber % MODULO_3 == 0 && inputNumber % MODULO_5 == 0 && inputNumber % MODULO_7 == 0 ){
-            return "FizzWhizzBuzz";
+            return FIZZ+WHIZZ+BUZZ;
         } else if(inputNumber % MODULO_3 == 0 && inputNumber % MODULO_5 == 0){
-            return FIZZ_BUZZ;
+            return FIZZ+BUZZ;
         } else if(inputNumber % MODULO_3 == 0 && inputNumber % MODULO_7 == 0){
-            return FIZZ_WHIZZ;
+            return FIZZ+WHIZZ;
         } else if(inputNumber % MODULO_3 == 0){
             return FIZZ;
         } else if(inputNumber % MODULO_5 == 0){
