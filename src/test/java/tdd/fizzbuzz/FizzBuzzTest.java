16037afd2ccdca7fb196fdb500782fcaa6a8eaa7
dominje <jericah.domingo@oocl.com>
2020-10-15 13:55:23 +0800

    [fizzbuzz][jericah]test: add test that will output Fizz when the input number is a multiple of three

--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,20 @@
 package tdd.fizzbuzz;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
+
+    @Test
+    void should_return_Fizz_when_multiple_of_three_given_order_number_is_three() {
+        // given
+        int inputNumber = 3;
+        FizzBuzz fizzBuzz = new FizzBuzz();
+
+        // when
+        String actual = fizzBuzz.countOff(inputNumber);
+
+        //then
+        assertEquals("Fizz", actual);
+    }
 }
