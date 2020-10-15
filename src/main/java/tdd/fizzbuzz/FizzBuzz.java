306d14a89b428dc93ebf893436c491aba65ad99f
dominje <jericah.domingo@oocl.com>
2020-10-15 14:08:56 +0800

    [fizzbuzz][jericah]refactor: replaced variables to constants, formatting, made the instantiation of FizzBuzz Class into a global variable

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -4,12 +4,14 @@ public class FizzBuzz {
 
     public static final String FIZZ = "Fizz";
     public static final int MODULO_3 = 3;
+    public static final String BUZZ = "Buzz";
+    public static final int MODULO_5 = 5;
 
     public String countOff(int inputNumber) {
-        if(inputNumber % MODULO_3 ==0){
+        if(inputNumber % MODULO_3 == 0){
             return FIZZ;
-        } else if(inputNumber % 5 == 0){
-            return "Buzz";
+        } else if(inputNumber % MODULO_5 == 0){
+            return BUZZ;
         }
         return "";
     }
--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,15 +1,17 @@
 package tdd.fizzbuzz;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class FizzBuzzTest {
 
+    FizzBuzz fizzBuzz = new FizzBuzz();
+
     @Test
     void should_return_Fizz_when_multiple_of_three_given_order_number_is_three() {
         // given
         int inputNumber = 3;
-        FizzBuzz fizzBuzz = new FizzBuzz();
 
         // when
         String actual = fizzBuzz.countOff(inputNumber);
@@ -22,7 +24,6 @@ public class FizzBuzzTest {
     void should_return_Buzz_when_multiple_of_five_fiven_order_number_is_five() {
         // given
         int inputNumber = 5;
-        FizzBuzz fizzBuzz = new FizzBuzz();
 
         // when
         String actual = fizzBuzz.countOff(inputNumber);
