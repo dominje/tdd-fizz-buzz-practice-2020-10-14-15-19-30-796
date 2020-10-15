ae27a41d11f49ecfc2c455ad032185879f772fdf
dominje <jericah.domingo@oocl.com>
2020-10-15 13:58:15 +0800

    [fizzbuzz][jericah]feature: add logic that will output Fizz when the input number is a multiple of three

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -1,4 +1,11 @@
 package tdd.fizzbuzz;
 
 public class FizzBuzz {
+
+    public String countOff(int inputNumber) {
+        if(inputNumber % 3 ==0){
+            return "Fizz";
+        }
+        return "";
+    }
 }
